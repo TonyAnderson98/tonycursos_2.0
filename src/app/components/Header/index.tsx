2e40dab09c9e08c6c8dfaa7913915aafaf75d247
Tony Anderson <anderson.tony.98@gmail.com>
2025-05-18 00:06:52 -0300

    feat: Corrige tags <a> por <Link>
    - Corrigidas as tags <a> por tags <Link>

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,13 +1,26 @@
 import styles from "./styles.module.css";
+import Link from "next/link";
 
 export default function Header() {
     return (
         <header className={styles.header}>
             <nav>
                 <ul className={styles.link__itens}>
-                    <li><a className={styles.link__item} href="/">Início</a></li>
-                    <li><a className={styles.link__item} href="/sobre">Sobre</a></li>
-                    <li><a className={styles.link__item} href="/contato">Contato</a></li>
+                    <li>
+                        <Link href="/" className={styles.link__item}>
+                            Início
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/sobre" className={styles.link__item}>
+                            Sobre
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/contato" className={styles.link__item}>
+                            Contato
+                        </Link>
+                    </li>
                     <li><span className={styles.link__item}>Olá, USER_NAME</span></li>
                     <li><span className={styles.link__item}>Sair</span></li>
                 </ul>
